3cea4a2a5427ac4707c5502fecc7e80b60cccde7
azetar <accsend876@gmail.com>
2025-02-11 15:27:22 +0800

    tambah homepage

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -54,17 +54,6 @@ class AdminPanelProvider extends PanelProvider
                 'warning' => '#ffbd03',
             ])
             ->sidebarCollapsibleOnDesktop()
-            // ->collapsedSidebarWidth('9rem')
-            //->topNavigation()
-
-
-
-            // NavigationItem::make('User')
-            // ->label(fn (): string => __('filament-panels::pages/auth/edit-profile.label'))
-            // ->url(fn () => EditProfile::getUrl())
-            // ->icon('heroicon-o-user-circle')
-            // ->isActiveWhen(fn (): bool => request()->routeIs(EditProfile::getRouteName())),
-
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\\Filament\\Resources')
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\\Filament\\Pages')
             ->pages([
